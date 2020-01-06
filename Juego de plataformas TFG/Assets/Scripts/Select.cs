e1f247289717eefe2cf9574574382ff0fb22619b
Ramón Jesús Torres Madrid <ramonjtorres1997@gmail.com>
2020-01-06 21:51:52 +0100

    Add some menus
    Menus fail, so I need to repair some buttons and add functionality

--- Juego de plataformas TFG/Assets/Scripts/Select.cs
+++ Juego de plataformas TFG/Assets/Scripts/Select.cs
@@ -8,9 +8,17 @@ public class Select : MonoBehaviour
     public GameObject chica;
     public GameObject chico;
 
+    SpriteRenderer SR;
+    public bool over;
+    public Color32 ColourNormal;
+    public Color32 ColourOver;
+
+    void Start(){
+
+        SR = GetComponent<SpriteRenderer>();
+    }
+
     void OnMouseDown() {
-        
-        if(Input.GetMouseButtonDown(0)){
 
             if(chica.activeSelf){
                 chica.SetActive(false);
@@ -20,6 +28,18 @@ public class Select : MonoBehaviour
                 chica.SetActive(true);
                 chico.SetActive(false);
             }
+    }
+
+    void Update(){
+
+        if(over == true){
+
+            SR.color = ColourOver;
+            over = false;
+        }
+        else{
+
+            SR.color = ColourNormal;
         }
     }
 }
