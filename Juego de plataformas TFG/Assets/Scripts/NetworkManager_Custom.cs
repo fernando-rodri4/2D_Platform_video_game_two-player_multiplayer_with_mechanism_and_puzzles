9dd8f2616b0c21e57fbf9455336c8705175cccb4
Ramón Jesús Torres Madrid <ramonjtorres1997@gmail.com>
2020-01-24 18:54:37 +0100

    Multiplayer finish
    Need to ignore colliders (between players) and a camera that follows the player

--- Juego de plataformas TFG/Assets/Scripts/NetworkManager_Custom.cs
+++ Juego de plataformas TFG/Assets/Scripts/NetworkManager_Custom.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.Networking.NetworkSystem;
 
 public class NetworkManager_Custom : NetworkManager
 {
+    public int chosenCharacter = 0;
+    public GameObject[] characters;
+
+    Select select = null;
+
+    void Start(){
+
+        select = GameObject.Find("player").GetComponent<Select>();
+        chosenCharacter = select.elect;
+    }
+
+    //subclass for sending network messages
+    public class NetworkMessage : MessageBase
+    {
+        public int chosenClass;
+    }
+
     public void StartupHost(){
 
         SetPort();
@@ -56,4 +74,41 @@ public class NetworkManager_Custom : NetworkManager
         GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.RemoveAllListeners();
         GameObject.Find("ButtonDisconnect").GetComponent<Button>().onClick.AddListener(NetworkManager.singleton.StopHost);
     }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader) {
+ 
+        NetworkMessage message = extraMessageReader.ReadMessage<NetworkMessage>();
+        int selectedClass = message.chosenClass;
+        Debug.Log("server add with message " + selectedClass);
+ 
+        GameObject player;
+        Transform startPos = GetStartPosition();
+ 
+        if(startPos != null)
+        {
+            player = Instantiate(characters[selectedClass], startPos.position,startPos.rotation)as GameObject;
+        }
+        else
+        {
+            player = Instantiate(characters[selectedClass], Vector3.zero, Quaternion.identity) as GameObject;
+ 
+        }
+ 
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        NetworkMessage test = new NetworkMessage();
+        test.chosenClass = chosenCharacter;
+ 
+        ClientScene.AddPlayer(conn, 0, test);
+    }
+ 
+ 
+    public override void OnClientSceneChanged(NetworkConnection conn)
+    {
+        //base.OnClientSceneChanged(conn);
+    }
+    
 }
--- Juego de plataformas TFG/Assets/Scripts/Select.cs
+++ Juego de plataformas TFG/Assets/Scripts/Select.cs
@@ -8,47 +8,29 @@ public class Select : MonoBehaviour
     public GameObject girl;
     public GameObject boy;
 
-    public GameObject elect1;
-    public GameObject elect2;
-
-    SpriteRenderer SR;
-    public bool over;
-    public Color32 ColourNormal;
-    public Color32 ColourOver;
+    public int elect;
 
     void Start(){
 
-        SR = GetComponent<SpriteRenderer>();
+        elect = 0;
+         DontDestroyOnLoad(this.gameObject);
     }
 
-    void OnMouseDown() {
+    public void OnMouseDown() {
 
             if(girl.activeSelf){
+                
                 girl.SetActive(false);
                 boy.SetActive(true);
 
-                elect1.SetActive(false);
-                elect2.SetActive(true);
+                elect = 1;
             }
             else if(boy.activeSelf){
+
                 girl.SetActive(true);
                 boy.SetActive(false);
 
-                elect1.SetActive(true);
-                elect2.SetActive(false);
+                elect = 0;
             }
-    }
-
-    void Update(){
-
-        if(over == true){
-
-            SR.color = ColourOver;
-            over = false;
-        }
-        else{
-
-            SR.color = ColourNormal;
-        }
     }
 }
